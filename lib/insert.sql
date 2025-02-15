a73d84e52c52f3b240606ac895987fabff76dc9b
Edmund <edmundgachanja@gmail.com>
2025-02-15 22:30:25 +0300

    Completes the lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate",10,"M","black","none",false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy",23,"M","black","none",true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha",103,"F","white","none",false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown",130,"M","black","none",true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa",20,"F","pink","none",false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch",14,"M","charcoal","none",false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy",12,"F","brown","none",true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL,0,"M","gold","none",false);
